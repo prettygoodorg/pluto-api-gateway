f0ccdfb809155c1ff85ae54e3660a4ca3a703716
wichan <wichan7@naver.com>
2026-04-10 21:39:22 +0900

    initial commit

---
+++ src/main/java/com/prettygoodorg/pluto_api_gateway/PlutoApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.prettygoodorg.pluto_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlutoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlutoApiGatewayApplication.class, args);
+	}
+
+}
